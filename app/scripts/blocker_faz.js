417bb234c9b8b643582dcc4eedbd68ef21262021
kagal <kagal@DESKTOP-799P09E>
2019-05-28 22:34:34 +0200

    Blocker added for faz.net.

---
+++ app/scripts/blocker_faz.js
@@ -0,0 +1,53 @@
+/**
+ * Copyright (c) 2019, kagal (https://github.com/keinepartei).
+ * 
+ * Bündnis90/Die Grünen Content Blocker Browser Plug-In
+ * (https://github.com/keinepartei/gruenen-blocker-plugin) is licensed under the
+ * GNU LGPL3.0 from 29 June 2007
+ * (https://www.gnu.org/licenses/lgpl-3.0.en.html).
+ * 
+ * Content blocker template for faz.net.
+ * 
+ * Original source code is from "AlkoholLiga" (https://github.com/AlkoholLiga)
+ * licensed under the ISC License by the Internet Systems Consortium
+ * (www.isc.org)
+ * https://github.com/AlkoholLiga/afd-blocker-plugin/blob/master/app/scripts/blocker_faz.js
+ * commit 64b1aa529ff68db576a523f4868f28529aeaffb4.
+ */
+console.log("#### NO PARTY CONTENT-BLOCKER ####");
+
+import { Blocker } from "./blocker_general";
+
+let blocker = new Blocker([
+    {
+        selector: 'div.o-Grid_Col',
+        type: 'big'
+    },
+    {
+        selector: 'div.atc-Text',
+        type: 'big'
+    },
+    {
+        selector: 'li.nvg-Breadcrumb_Item',
+        type: 'small'
+    },
+    {
+        selector: 'div.Teaser620',
+        type: 'big'
+    },
+    {
+        selector: 'div.gh-MainNav_Article',
+        type: 'small'
+    },
+    {
+        selector: 'div.ctn-PlaceholderContent',
+        type: 'big'
+    },
+    {
+        selector: 'div.sld-TeaserMore_Item',
+        type: 'big'
+    },
+]);
+
+blocker.modifyContent([document]);
+blocker.watchPageForMutations();
