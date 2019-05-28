b2c3cc20d7c90d7f5a15332772b4cb5062fcf149
kagal <kagal@DESKTOP-799P09E>
2019-05-28 22:38:32 +0200

    Blocker added for taz.de.

---
+++ app/scripts/blocker_taz.js
@@ -0,0 +1,49 @@
+/**
+ * Copyright (c) 2019, kagal (https://github.com/keinepartei).
+ * 
+ * Bündnis90/Die Grünen Content Blocker Browser Plug-In
+ * (https://github.com/keinepartei/gruenen-blocker-plugin) is licensed under the
+ * GNU LGPL3.0 from 29 June 2007
+ * (https://www.gnu.org/licenses/lgpl-3.0.en.html).
+ * 
+ * Content blocker template for taz.de.
+ * 
+ * Original source code is from "AlkoholLiga" (https://github.com/AlkoholLiga)
+ * licensed under the ISC License by the Internet Systems Consortium
+ * (www.isc.org)
+ * https://github.com/AlkoholLiga/afd-blocker-plugin/blob/master/app/scripts/blocker_faz.js
+ * commit aa40a331ef5e2e1a66c1f057008ec68640e77fe1.
+ */
+console.log("#### NO PARTY CONTENT-BLOCKER ####");
+
+import { Blocker } from "./blocker_general";
+
+let blocker = new Blocker([
+    {
+        selector: 'article.sectbody',
+        type: 'big'
+    },
+    {
+        selector: 'div.odd.sect.sect_description',
+        type: 'big'
+    },
+    {
+        selector: 'div.even.sect.sect_tags',
+        type: 'small'
+    },
+    {
+        selector: '.objlink.report.article',
+        type: 'big'
+    },
+    {
+        selector: '.objlink.subjective.commentary.article',
+        type: 'small'
+    },
+    {
+        selector: '.objlink.brief.legacy.article',
+        type: 'big'
+    },
+]);
+
+blocker.modifyContent([document]);
+blocker.watchPageForMutations();
