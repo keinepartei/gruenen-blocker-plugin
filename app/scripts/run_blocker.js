4aff04126814b6eb8061f37c069e5a0636da750b
kagal <kagal@DESKTOP-799P09E>
2019-05-30 23:44:52 +0200

    Fixed license header.

--- app/scripts/run_blocker.js
+++ app/scripts/run_blocker.js
@@ -1,5 +1,12 @@
 /**
+ * Copyright (c) 2019, kagal "keinepartei" (https://github.com/keinepartei).
  * 
+ * Gets the XPath expression from storage built by users options and invokes the
+ * blocker.
+ * 
+ * Published under the GNU Lesser General Public License Version 3, 29 June 2007
+ * (see: https://www.gnu.org/licenses/lgpl-3.0.en.html) and see LICENSE
+ * (https://github.com/keinepartei/gruenen-blocker-plugin/LICENSE).
  */
 module.exports = {
 	run: function(blocker) {
