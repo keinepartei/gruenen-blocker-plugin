8bf7fc697c14c5922743aa9ebdb793e01fe1464f
kagal <kagal@DESKTOP-799P09E>
2019-06-01 16:35:44 +0200

    Blocker added for zeit.de

--- app/scripts/blocker_zeit.js
+++ app/scripts/blocker_zeit.js
@@ -6,7 +6,7 @@
  * GNU LGPL3.0 from 29 June 2007
  * (https://www.gnu.org/licenses/lgpl-3.0.en.html).
  * 
- * Content blocker template for n-tv.de.
+ * Content blocker template for zeit.de.
  * 
  * Original source code is from "dieparteidiepartei"
  * (https://github.com/dieparteidiepartei) licensed under the ISC License by the
@@ -17,12 +17,12 @@ console.log("Call content blocker.");
 import { Blocker } from "./blocker";
 
 let blocker = new Blocker([
-    { // Almost all.
-        selector: 'article.teaser',
-        type: 'big'
+    { // 'Schwerpunkte' bordered links at the top of the page.
+        selector: 'a.nav__tag',
+        type: 'small'
     },
-    { // Section 'Meistgelesen'.
-        selector: 'li.teaser',
+    { 
+        selector: 'article.teaser-small',
         type: 'small'
     },
 ]);
